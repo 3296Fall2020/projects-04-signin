8ff22797e3ea20b3deedc2496aaed878e8b0bb99
Madison Stock <maddiemarie1998@gmail.com>
2020-11-17 21:52:48 -0500

    Added another data field

--- webapp/app.py
+++ webapp/app.py
@@ -10,11 +10,12 @@ app = Flask(__name__)
 # additional input parameter booleans
 add_dob = False
 add_gender = False
+add_organization = False
 
 # HOME LOGIN PAGE
 @app.route('/')
 def home_page():
-    return render_template('index.html', add_dob = add_dob, add_gender = add_gender)
+    return render_template('index.html', add_dob = add_dob, add_gender = add_gender, add_organization = add_organization)
 
 # LOGIN FORM SUBMISSION
 @app.route('/', methods=['POST'])
@@ -42,24 +43,29 @@ def submit():
     if 'gender' in request.form:
         login.gender = request.form['gender']
 
+    # organization
+    if 'organization' in request.form:
+        login.gender = request.form['organization']
+
     # add user input to the database
     submissionStatus = add_login(login)
 
     # return index html with parameters that show the form was submitted correctly
-    return render_template('index.html', submitted=True, submissionStatus = submissionStatus, add_dob = add_dob, add_gender = add_gender)
+    return render_template('index.html', submitted=True, submissionStatus = submissionStatus, add_dob = add_dob, add_gender = add_gender, add_organization = add_organization)
 
 # CONFIG PAGE
 @app.route('/config')
 def config_page():
     config = get_config()
 
-    return render_template('config.html', add_dob = config.add_dob, add_gender = config.add_gender)
+    return render_template('config.html', add_dob = config.add_dob, add_gender = config.add_gender), add_organization = config.add_organization)
 
 # CONFIG FORM SUBMISSION PAGE
 @app.route('/config', methods=['POST'])
 def config_page_submit():
     global add_gender
     global add_dob
+    global add_organization
 
     # ininitalize config info object
     config_data = config_info(None, None, None)
@@ -76,10 +82,16 @@ def config_page_submit():
     else:
         add_dob = False
 
+    if 'add_organization' in request.form:
+        add_organization = True
+        config_data.add_organization = "on"
+    else:
+        add_organization = False
+
     # add config to the database
     add_config(config_data)
 
-    return render_template('config.html', add_dob = add_dob, add_gender = add_gender)
+    return render_template('config.html', add_dob = add_dob, add_gender = add_gender, add_organization = add_organization)
 
 def run_app():
     app.run(debug=True)
